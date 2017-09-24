7f6db74dff3c1accb6fe7d6c829f2d831b850e23
kimimaro <kimimaro1952@hotmail.com>
2017-09-24 18:01:46 +0700

    Nid edit User interface

---
+++ include/site/user_del.php
@@ -0,0 +1,13 @@
+<?php
+	include_once('../config/routeros_api.class.php');			
+	include_once('conn.php');
+	echo "<meta charset='utf-8'>" ;
+	$username=$_GET['user'];	
+	$ARRAY = $API->comm("/ip/hotspot/user/remove
+						=.id=".$_GET['user']."");	
+		
+	echo "<script>alert('Delete Successful.')</script>";
+	echo "<meta http-equiv='refresh' content='0;url=index.php?opt=user' />";
+	exit;
+
+?>
